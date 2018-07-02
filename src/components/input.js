998670134fd7618275a25fe67667b63cd0e55e31
Andrew Ward <ward.andrew235@gmail.com>
2018-07-02 11:13:28 -0600

    added card styles

--- src/components/input.js
+++ src/components/input.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const Input = () => {
-    render() {
         return(
             <div className="input">
                 <input />
                 <label>Color</label>
             </div>
         )
-    }
-}
+};
 
 export default Input;
