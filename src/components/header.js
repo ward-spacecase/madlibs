83f16550729c6b1adba44e9d9805e1e762b557e3
Andrew Ward <ward.andrew235@gmail.com>
2018-07-10 10:03:51 -0600

    background skew added

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Header = (title, subtitle) => {
     return(
       <div className="header">
+          <div className="header-skew"></div>
+          <div className="header-border"></div>
           <h1>{title}</h1>
           <p>{subtitle}</p>
       </div>
